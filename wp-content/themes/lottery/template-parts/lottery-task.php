911c58d20761afa7ccc143b0c9f7a09272d1429c
Lev <lev.makarenko.1987@gmail.com>
2016-07-05 17:47:57 +0500

    Progress

--- wp-content/themes/lottery/template-parts/lottery-task.php
+++ wp-content/themes/lottery/template-parts/lottery-task.php
@@ -13,10 +13,9 @@
                         </div>
                                 <div class="row">
                                             <div class="pre-ready lottery-task-btn lottery-task-btn-start" <?php if ($is_user){ ?>style="display:none;"<?php } ?>>
-                                                    <a title="Участвовать" href="<?php if ($is_user){ ?><?php echo $GLOBALS['wasd_domain']; ?>/soctask/user/starttask/id/<?php echo $t['id']; ?><?php } else { echo $GLOBALS['wasd_domain']; } ?>" target="_blank" class="button">
+                                                    <a title="Участвовать" href="<?php if ($is_user){ ?><?php echo $GLOBALS['wasd_domain']; ?>/soctask/user/starttask/id/<?php echo $t['id']; ?><?php } else { echo $GLOBALS['wasd_domain']; } ?>" <?php if ($is_user){ ?>target="_blank"<?php } ?> class="button">
                                                             <div class="ready pc">Участвовать</div>
                                                     </a>
-                                                <!-- {$captcha|default:''} -->
                                             </div>
                                             <?php if ($is_user){ ?>
                                             <div class="pre-ready lottery-task-btn lottery-task-btn-loading">
@@ -48,33 +47,6 @@
         <li class="task-c task-c-cpa" data-task-id="<?php echo $t['id']; ?>" data-task-type="<?php echo $t['task_type']; ?>" data-adv-id="<?php echo $t['adv_camp']; ?>" id="cpa_task_<?php echo $t['id']; ?>">
                         <div class="img-holder">
                                 <img src="<?php echo $GLOBALS['wasd_domain']; ?>/public/jobs/big_<?php echo $t['img']; ?>" alt="<?php echo $t['header']; ?>">
-                        <!--        
-                        {if $t.pay_variant=='fix'}
-                            {if $t.only_apple}
-                            <div class="badge os">
-                                <div>
-                                    <img src="/site/skins/wasd2_sub/public/images/iOS.png">
-                                    {if $t.platform_ios}iPhone {/if}{if $t.platform_ipod}iPod {/if}{if $t.platform_ipad}iPad{/if}
-                                </div>
-                            </div>
-                            {elseif $t.only_android}
-                            <div class="badge os">
-                                <div>
-                                    <img src="/site/skins/wasd2_sub/public/images/android.png">
-                                    Android
-                                </div>
-                            </div>
-                            {/if}
-                            {if !$t.pay_refer}
-                                <div class="badge os no_ref">
-                                    <div>
-                                        <img src="/site/skins/wasd2_sub/public/images/no_ref.png">
-                                        Без реферальных выплат
-                                    </div>
-                                </div>
-                            {/if}
-                        {/if}
-                        -->
                         </div>
                         <div class="description">
                                 <h2><?php echo $t['header']; ?></h2>
@@ -100,43 +72,9 @@
                                         <div class="ready pc">Требуется авторизация</div>
                                     </a>
                                 <?php } else { ?>
-                                    <!--
-                                    {if $t.only_apple}
-                                        {if isset($is_apple) && $is_apple === true}
-                                            <a title="Участвовать" href="/adv/index/jump/link/{$t.id}" target="_blank" onclick="{if $is_user}{if $t.captcha} showRecaptcha('{$publickey}',startCpaTask,{$t.id},'/adv/index/jump/link/{$t.id}'); return false; {else} startCpaTask({$t.id}); {/if}{else}showDemoLogin();return false;{/if}" class="button">
-                                                <div class="ready apple">Участвовать</div>
-                                            </a>
-                                        {else}
-                                            <a title="Участвовать" href="#" onclick="{if $is_user}showOnlyAppleDialog(); return false;{else}showDemoLogin();return false;{/if}" class="button">
-                                                <div class="ready apple">Участвовать</div>
-                                            </a>
-                                        {/if}
-                                    {elseif $t.only_android}
-                                        {if isset($is_android) && $is_android === true}
-                                            <a title="Участвовать" href="/adv/index/jump/link/{$t.id}" target="_blank" onclick="{if $is_user}{if $t.captcha} showRecaptcha('{$publickey}',startCpaTask,{$t.id},'/adv/index/jump/link/{$t.id}'); return false; {else} startCpaTask({$t.id}); {/if}{else}showDemoLogin();return false;{/if}" class="button android">
-                                                <div class="ready android">Участвовать</div>
-                                            </a>
-                                        {else}
-                                            <a title="Участвовать" href="#" onclick="{if $is_user}showOnlyAndroidDialog(); return false;{else}showDemoLogin();return false;{/if}" class="button android">
-                                                <div class="ready android">Участвовать</div>
-                                            </a>
-                                        {/if}
-                                    {elseif $t.only_ipod}
-                                        {if isset($is_ipod) && $is_ipod === true}
-                                            <a title="Участвовать" href="/adv/index/jump/link/{$t.id}" target="_blank" onclick="{if $is_user}{if $t.captcha} showRecaptcha('{$publickey}',startCpaTask,{$t.id},'/adv/index/jump/link/{$t.id}'); return false; {else} startCpaTask({$t.id}); {/if}{else}showDemoLogin();return false;{/if}" class="button">
-                                                <div class="ready apple">Участвовать</div>
-                                            </a>
-                                        {else}
-                                            <a title="Участвовать" href="#" onclick="{if $is_user}showOnlyiPodDialog(); return false;{else}showDemoLogin();return false;{/if}" class="button">
-                                                <div class="ready apple">Участвовать</div>
-                                            </a>
-                                        {/if}
-                                    {else}
-                                    -->
-                                        <a title="Участвовать" href="<?php echo $GLOBALS['wasd_domain']; ?>/adv/index/jump/link/<?php echo $t['id']; ?>" target="_blank" onclick="<?php if ($is_user){ ?><?php if ($t['captcha']){ ?> showRecaptcha('publickey',startCpaTask,<?php echo $t['id']; ?>,'/adv/index/jump/link/<?php echo $t['id']; ?>'); return false; <?php } else { ?> startCpaTask(<?php echo $t['id']; ?>); <?php } } else { ?>showDemoLogin();return false;<?php } ?>" class="button">
+                                        <a title="Участвовать" href="<?php if ($is_user){ echo $GLOBALS['wasd_domain']; ?>/adv/index/jump/link/<?php echo $t['id']; } else { ?>#<?php } ?>" <?php if ($is_user){ ?>target="_blank"<?php } ?> class="button">
                                             <div class="ready pc">Участвовать</div>
                                         </a>
-                                    <!-- {/if} -->
                                 <?php } ?>
                             <?php } ?>
                                                 </div>
