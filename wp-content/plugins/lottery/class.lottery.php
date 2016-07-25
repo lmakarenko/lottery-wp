397b32367fcb4dce44b0aa7c5b993b3ba92e83fc
Lev <lev.makarenko.1987@gmail.com>
2016-07-25 13:37:21 +0500

    Test for ajax external

--- wp-content/plugins/lottery/class.lottery.php
+++ wp-content/plugins/lottery/class.lottery.php
@@ -678,7 +678,7 @@ class lottery {
             wp_send_json($data);
             return false;
         }
-        $data['html'] = file_get_contents($GLOBALS['wasd_domain'] . '/api/login/ajaxform');
+        $data['data'] = file_get_contents($GLOBALS['wasd_domain'] . '/api/login/ajaxform');
         wp_send_json($data);
     }
     
