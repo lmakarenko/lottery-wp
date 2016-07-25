7694df34cfaa6bc49c6c19276f8143babddc1646
Lev <lev.makarenko.1987@gmail.com>
2016-07-25 14:04:05 +0500

    Test for ajax external

--- wp-content/themes/lottery/index.php
+++ wp-content/themes/lottery/index.php
@@ -16,8 +16,10 @@ if ( 'ACTIVE' == lottery_get_status() && 0 < count($posts) ) :
         }
 ?>
 <input type="hidden" id="id-posts" name="id_posts" value="<?php echo lottery_get_posts_ids_s(); ?>" />
+<?php if($GLOBALS['user_data']){ ?>
 <input type="hidden" id="id-adv" name="id_adv" value="<?php echo lottery_get_adv_ids_s(); ?>" />
 <script type="text/javascript" src="/wp-content/themes/lottery/js/lottery-index.js"></script>
+<?php } ?>
 <script type="text/javascript" src="/wp-content/themes/lottery/js/lottery-complete.js"></script>
 <?php
 else :
