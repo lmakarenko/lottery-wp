8ddb2d63e3dffe4282ba2d9136236816c080f715
Lev <lev.makarenko.1987@gmail.com>
2016-07-26 17:03:57 +0500

    banners off

--- wp-content/themes/lottery/header.php
+++ wp-content/themes/lottery/header.php
@@ -95,7 +95,6 @@
         <div id="wrapper">
             <div class="w1">
             <div class="ad" style="height: 120px;">
-                <?php /*
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <!-- Wasd TOP -->
                 <ins class="adsbygoogle"
@@ -104,7 +103,7 @@
                      data-ad-slot="7115443322"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script> */ ?>
+                </script>
             </div>
                         <div id="header">
                                 <div class="nav-holder">
