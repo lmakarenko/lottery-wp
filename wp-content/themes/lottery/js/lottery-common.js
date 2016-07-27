3540b318155a39f40d21a7d4c257f5bad328fc2c
Lev <lev.makarenko.1987@gmail.com>
2016-07-27 13:31:39 +0500

    auth ajax

--- wp-content/themes/lottery/js/lottery-common.js
+++ wp-content/themes/lottery/js/lottery-common.js
@@ -1,7 +1,6 @@
 function sendPost(url,data,success,fail) {
         var d = {};
-        d['data'] = data;
-        d['data']['url'] = url;
+        d['data'] = data + '&url=' + url;
         d['action'] = 'wasd_call';
         d['ajax_nonce'] = asdfqwer;
         return $.post('/wp-admin/admin-ajax.php',d,function(ret) {
