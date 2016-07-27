d2397dc82f5c9791097a31a18ea971b7751745c5
Lev <lev.makarenko.1987@gmail.com>
2016-07-27 13:09:51 +0500

    auth ajax

--- wp-content/plugins/lottery/inc/ajax_login_form.php
+++ wp-content/plugins/lottery/inc/ajax_login_form.php
@@ -66,6 +66,24 @@
 </div>
 <script>
     
+    function sendPost(url,data,success,fail) {
+            var d = {};
+            data['url'] = url;
+            d['data'] = data;
+            d['action'] = 'wasd_call';
+            d['ajax_nonce'] = asdfqwer;
+            return $.post('/wp-admin/admin-ajax.php',d,function(ret) {
+                    if (ret['error']!='') {
+                            alert(ret['error']);
+                            if (fail!==undefined) fail(ret);
+                            return false;
+                    }else {
+                            if (success!==undefined) success(ret);
+                            return true;
+                    }
+            },'json');
+    }
+    
     $("#exit-form-back").click(function () {
         $("#exit-form-back").css({"background-color": "", "opacity": ""});
         $("#exit-form-back").hide();
