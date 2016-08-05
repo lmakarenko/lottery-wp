710114502cb074e537db9a7616867d4c7c11abc3
Lev <lev.makarenko.1987@gmail.com>
2016-08-05 17:22:09 +0500

    lottery report column added

--- wp-content/themes/lottery/js/lottery-admin.js
+++ wp-content/themes/lottery/js/lottery-admin.js
@@ -1,6 +1,6 @@
 jQuery('document').ready(function(){
    jQuery('.lottery-report-btn').on('click', function(e){
-        var post_id = parseInt($(this).attr('data-id'));
+        var post_id = parseInt(jQuery(this).attr('data-id'));
         jQuery.ajax({
             type:'post',
             url: ajax_url,
