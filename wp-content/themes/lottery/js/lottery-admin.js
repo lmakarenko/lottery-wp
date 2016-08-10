bf82a51a94bae23588e362c9241c6a98b5958c77
Lev <lev.makarenko.1987@gmail.com>
2016-08-10 14:39:02 +0500

    jQuery fileDownload removed

--- wp-content/themes/lottery/js/lottery-admin.js
+++ wp-content/themes/lottery/js/lottery-admin.js
@@ -101,16 +101,7 @@ jQuery('document').ready(function(){
                        dataType: 'json',
                        success: function(data){
                             if(data.url){
-                                jQuery.fileDownload(data.url, {
-                                    successCallback: function (url) {
-                                        alert('You just got a file download dialog or ribbon for this URL :' + url);
-                                    },
-                                    failCallback: function (html, url) {
-                                        alert('Your file download just failed for this URL:' + url + '\r\n' +
-                                                'Here was the resulting error HTML: \r\n' + html
-                                        );
-                                    }
-                                });
+                                window.location.href = data.url;
                            }
                        },
                        complete: function(){
