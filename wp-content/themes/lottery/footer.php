2bf08fb268102b21148e216b6607d28557f42a97
Lev <lev.makarenko.1987@gmail.com>
2016-08-18 19:37:35 +0500

    google analytics

--- wp-content/themes/lottery/footer.php
+++ wp-content/themes/lottery/footer.php
@@ -90,14 +90,13 @@
 </a>
 
 <script type="text/javascript" src="/wp-content/themes/lottery/js/lottery-history.js"></script>
-
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-34452323-2', 'auto');
+  ga('create', 'UA-71682761-2', 'auto');
   ga('send', 'pageview');
 
 </script>
