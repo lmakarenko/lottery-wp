ebf5cd132295da9a27288159ce8203fe4c56ef4d
Lev <lev.makarenko.1987@gmail.com>
2016-08-22 12:30:51 +0500

    404 tpl

--- wp-content/themes/lottery/404.php
+++ wp-content/themes/lottery/404.php
@@ -1,34 +1,11 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
-
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
+<?php get_header(); ?>
+<link rel="stylesheet" type="text/css" href="/site/plugins/error/public/css/controller/index/error.css" />
+<div id="main">
+    <div class="error_box">
+        <div class="error_header">Опять всё сломал?</div>
+        <div class="error_text">Ошибка 404</div>
+        <div class='error_desc'>Страница не найдена</div>
+        <a href='/'>Перейти на главную страницу.</a>
+    </div>
+</div>
 <?php get_footer(); ?>
