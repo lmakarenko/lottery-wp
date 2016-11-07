277c7e8010dfe893c29bbdd631f0d93e28bc8fcc
lmakarenko <r59makarenko@gmail.com>
2016-11-07 13:23:27 +0500

    vk error alert

--- wp-content/themes/lottery/single.php
+++ wp-content/themes/lottery/single.php
@@ -104,7 +104,7 @@ $(function(){
 <div class="exit-form-back exit-form-back1">
     <div class="lottery-history-alert lottery-history-alert1">
         <div class="lottery-history-alert1-inner">
-            В данный момент авторизация через VK недоступна, ведутся тех. работы. Для восстановления доступа обратитесь, пожалуйста, в тех. поддержку <a href="www.wasdclub.com/support">www.wasdclub.com/support</a>, либо ожидайте восстановления авторизации.
+            В данный момент авторизация через VK недоступна, ведутся тех. работы. Для восстановления доступа обратитесь, пожалуйста, в тех. поддержку <a href="http://www.wasdclub.com/support">www.wasdclub.com/support</a>, либо ожидайте восстановления авторизации.
         </div>
     </div>
 </div>
