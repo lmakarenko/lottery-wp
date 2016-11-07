47f716cce2cd98f01e02017716c5edf1f211bc9b
lmakarenko <r59makarenko@gmail.com>
2016-11-07 13:08:58 +0500

    vk error alert

--- wp-content/themes/lottery/single.php
+++ wp-content/themes/lottery/single.php
@@ -45,6 +45,70 @@ if(0 < count($posts)){
 <input type="hidden" id="id-posts" name="id_posts" value="<?php echo lottery_get_posts_ids_s(); ?>" />
 <input type="hidden" id="id-adv" name="id_adv" value="<?php echo lottery_get_adv_ids_s(); ?>" />
 <script type="text/javascript" src="/wp-content/themes/lottery/js/lottery-single.js"></script>
+
+<style type="text/css">
+.lottery-history-alert1{
+    width: 600px;
+    text-align: justify;
+}
+.lottery-history-alert1-inner{
+    position: relative;
+    border-bottom: 1px solid #fff;
+    padding: 20px;
+    font-size: 16px;
+    font-weight:bold;
+    line-height: 24px;
+    background: rgba(255,255,255, 1.0);
+}
+</style>
+<script type="text/javascript">
+$(function(){
+   
+   function calcScrollTop_(){
+        var st = $(document).scrollTop(), wh = $(window).height(), eh = 115, mt = st;
+        if(wh > eh){
+            mt = st + Math.ceil((wh - eh) / 2);  
+        } else if(eh > wh){
+            mt = st + Math.ceil((eh - wh) / 2);
+        }
+        return mt;
+    }
+   
+   $('.lottery-task-btn-alert').on('click', function(){
+        anim_complete = false;
+        var $this = $(this),
+            e_c = $('.exit-form-back1').first(),
+            e_ = $('.lottery-history-alert1').first();
+        e_c.on('click', function(e){
+            $('.lottery-history-alert1').first().fadeOut(0, 'swing', function(){
+                $('.exit-form-back1').first().hide();
+                anim_complete = true;
+            }).off('scroll');
+        });
+        if(!$('.exit-form-back1').length){
+            e_c.css({'height': $(document).height() + 'px'});
+            e_.css({'margin-top': calcScrollTop_() + 'px'}).on('click', function(e){
+                e.stopPropagation();
+            });
+            e_c.show();
+            $(window).on('scroll', {'el': e_}, function(e){
+                e.data.el.css({'margin-top': calcScrollTop_() + 'px'});
+            });
+        }
+        e_.fadeIn(600, 'swing', function(){
+            anim_complete = true;
+        });
+   });
+});
+</script>
+<div class="exit-form-back exit-form-back1">
+    <div class="lottery-history-alert lottery-history-alert1">
+        <div class="lottery-history-alert1-inner">
+            В данный момент авторизация через VK недоступна, ведутся тех. работы. Для восстановления доступа обратитесь, пожалуйста, в тех. поддержку <a href="www.wasdclub.com/support">www.wasdclub.com/support</a>, либо ожидайте восстановления авторизации.
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
 <?php
 /*
